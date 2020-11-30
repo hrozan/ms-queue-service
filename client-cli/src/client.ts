cc86adf91288b2b43fd3434031477932e1e5cee9
lucaslfranco <llucasrgt@gmail.com>
2020-11-29 21:53:56 -0300

    refactor: rename project directory

---
+++ client-cli/src/client.ts
@@ -0,0 +1,23 @@
+import { io, Socket } from "socket.io-client"
+
+export class MSQSClient {
+    private socket: Socket
+
+    constructor(socket: Socket) {
+        this.socket = socket
+        this.socket.on("connection", this.onConnection)
+        socket.emit('send-message', { message: 'test', queue: 'a1' });
+    }
+
+    onConnection() {
+        console.log("client connected")
+    }
+}
+
+export const createMSQSClient = () =>
+    new Promise((resolve) => {
+        const socket = io("http://localhost:2307")
+        socket.connect()
+        console.log(socket.connected)
+        resolve(new MSQSClient(socket))
+    })
